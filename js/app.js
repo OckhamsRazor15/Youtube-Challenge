a927a3446ecf4d59064b97dc11e8d3ad64101c1e
Sako Kassabian <skassabian@csmci.com>
2015-12-20 17:15:32 -0800

    fixes getJSON

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(function(){
+	$("#search").submit(function(e){
+		e.preventDefault();
+		var searchTerm = $("#query").val();
+		getRequest(searchTerm);
+	});
+
+	function getRequest(searchTerm) {
+		var params = {
+			part: "snippet",
+			key: "AIzaSyBhjtDGFP2io6tZ40hqCUXrNkEfJzpzpFg",
+			q: searchTerm
+		};
+		url = "https://www.googleapis.com/youtube/v3/search";
+		
+		$.getJSON(url, params, function(data){
+			//console.log(data.items[0].snippet.thumbnails);
+			showResults(data.items);
+		});
+	}
+
+	function showResults(results) {
+		var html = "";
+		$.each(results, function(index, value){
+			console.log(results[index].snippet.thumbnails.medium);
+			html += "<img src='" + results[index].snippet.thumbnails.medium.url + "'>";
+			//console.log(value.Title);
+		});
+		$('#search-results').html(html);
+	}
+});
